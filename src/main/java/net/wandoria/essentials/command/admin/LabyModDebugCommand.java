0986e9345595ec76b8d328d1d219a314a9f55d4c
einjojo Johannes <johannes@einjojo.it>
2025-12-30 16:56:13 +0100

    Refactor LabyMod debug command to use asynchronous addon requests and improve addon disabling logic

--- src/main/java/net/wandoria/essentials/command/admin/LabyModDebugCommand.java
+++ src/main/java/net/wandoria/essentials/command/admin/LabyModDebugCommand.java
@@ -4,7 +4,6 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.labymod.serverapi.server.bukkit.LabyModProtocolService;
-import net.labymod.serverapi.server.bukkit.LabyModServerAPIPlugin;
 import net.wandoria.essentials.util.Text;
 import org.bukkit.entity.Player;
 import org.incendo.cloud.annotations.Command;
@@ -26,26 +25,29 @@ public class LabyModDebugCommand {
         }
         source.source().sendMessage(Text.deserialize("<prefix> <p>LabyMod Players:</p>"));
         for (var player : labyPlayers) {
-            source.source().sendMessage(Text.deserialize("<gray> - <hl><name></hl> <dark_gray>(Version: <hl><version></hl>)</dark_gray>",
-                    Placeholder.unparsed("version", player.getLabyModVersion()),
-                    Placeholder.unparsed("name", player.getPlayer().getName())));
-            var addons = player.installedAddons().getInstalledAddons();
-            if (addons.isEmpty()) {
-                source.source().sendMessage(Text.deserialize("     <gray>Keine Addons."));
-                continue;
-            }
-            source.source().sendMessage(Text.deserialize("     <h2>Addons:"));
-            for (var addon : addons) {
-                Component disableButton = addon.isEnabled() ? Component.text("[X]", Text.ERROR_COLOR)
-                        .clickEvent(ClickEvent.runCommand("/essentials labymod disableaddon " + player.getPlayer().getName() + " " + addon.getNamespace()))
-                        : Component.empty();
-                source.source().sendMessage(Text.deserialize("     <gray>- <active> <hl><namespace></hl> <dark_gray>(Version: <hl><version></hl>)</dark_gray> <button>",
-                        Placeholder.component("button", disableButton),
-                        Placeholder.unparsed("active", addon.isEnabled() ? "<green>●" : "<red>●"),
-                        Placeholder.unparsed("namespace", addon.getNamespace()),
-                        Placeholder.unparsed("version", addon.getVersion().toString())
-                ));
-            }
+            player.requestInstalledAddons(addonsResp -> {
+                source.source().sendMessage(Text.deserialize("<gray> - <hl><name></hl> <dark_gray>(Version: <hl><version></hl>)</dark_gray>",
+                        Placeholder.unparsed("version", player.getLabyModVersion()),
+                        Placeholder.unparsed("name", player.getPlayer().getName())));
+                var addons = addonsResp.getInstalledAddons();
+                if (addons.isEmpty()) {
+                    source.source().sendMessage(Text.deserialize("     <gray>Keine Addons."));
+                    return;
+                }
+                source.source().sendMessage(Text.deserialize("     <h2>Addons:"));
+                for (var addon : addons) {
+                    Component disableButton = addon.isEnabled() ? Component.text("[X]", Text.ERROR_COLOR)
+                            .clickEvent(ClickEvent.runCommand("/essentials labymod disableaddon " + player.getPlayer().getName() + " " + addon.getNamespace()))
+                            : Component.empty();
+                    source.source().sendMessage(Text.deserialize("     <gray>- <active> <hl><namespace></hl> <dark_gray>(Version: <hl><version></hl>)</dark_gray> <button>",
+                            Placeholder.component("button", disableButton),
+                            Placeholder.parsed("active", addon.isEnabled() ? "<green>●" : "<red>●"),
+                            Placeholder.unparsed("namespace", addon.getNamespace()),
+                            Placeholder.unparsed("version", addon.getVersion().toString())
+                    ));
+                }
+            });
+
         }
     }
 
@@ -53,14 +55,14 @@ public class LabyModDebugCommand {
     @Command("essentials labymod disableaddon <player> <addon>")
     @CommandDescription("Disables a LabyMod addon for a specific player")
     @Permission("essentials.admin.labymod.disableaddon")
-    public void disableLabyModAddon(Source source, Player player, String addonNamespace) {
+    public void disableLabyModAddon(Source source, Player player, String addon) {
         var labyPlayer = LabyModProtocolService.get().getPlayer(player.getUniqueId());
         if (labyPlayer == null) {
             source.source().sendMessage(Text.deserialize("<prefix> <red>Der Spieler <hl>" + player.getName() + "</hl> hat LabyMod nicht installiert."));
             return;
         }
-        labyPlayer.disableAddons(addonNamespace);
-        source.source().sendMessage(Text.deserialize("<prefix> <green>Das Addon <hl>" + addonNamespace + "</hl> wurde für Spieler <hl>" + player.getName() + "</hl> deaktiviert."));
+        labyPlayer.disableAddons(addon);
+        source.source().sendMessage(Text.deserialize("<prefix> <green>Das Addon <hl>" + addon + "</hl> wurde für Spieler <hl>" + player.getName() + "</hl> deaktiviert."));
     }
 
 
