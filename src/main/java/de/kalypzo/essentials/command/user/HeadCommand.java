2746432840fa8c02e25d665e4c4500e383f7d8be
einjojo Johannes <johannes@einjojo.it>
2026-04-12 12:30:36 +0200

    feat(head): implement cooldown check for head command
    - Added cooldown management for the head command.
    - Refactored giveHead and giveOwnHead methods to include cooldown logic.
    - Updated permissions for head command usage.

--- src/main/java/de/kalypzo/essentials/command/user/HeadCommand.java
+++ src/main/java/de/kalypzo/essentials/command/user/HeadCommand.java
@@ -1,15 +1,14 @@
 package de.kalypzo.essentials.command.user;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
-import de.kalypzo.essentials.command.CommandLoader;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import de.kalypzo.essentials.EssentialsPlugin;
+import de.kalypzo.essentials.command.CommandLoader;
 import de.kalypzo.essentials.user.cooldown.RedisCooldownManager;
 import de.kalypzo.essentials.util.MainThreadUtil;
 import de.kalypzo.essentials.util.PermissionsRange;
 import de.kalypzo.essentials.util.Text;
-import org.bukkit.Bukkit;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
@@ -32,14 +31,29 @@ import java.util.concurrent.CompletableFuture;
 @CommandContainer
 public class HeadCommand {
 
-    public HeadCommand() {
-        Bukkit.getPluginManager().addPermission(new org.bukkit.permissions.Permission("essentials.cooldown.head.bypass"));
-    }
-
     private static class LAZY {
         private static final RedisCooldownManager INSTANCE = new RedisCooldownManager(EssentialsPlugin.instance().getRedis().connect(), "head");
     }
 
+
+    /**
+     * Gives the player a head of the specified player
+     *
+     * @param source bypasses cooldown, if it has permission essentials.cooldown.head.bypass
+     * @return a future completing, when the head has been given
+     */
+    @Command("head|skull")
+    @CommandDescription("Gibt dir deinen eigenen Kopf")
+    @Permission("essentials.command.head.self")
+    public CompletableFuture<Void> giveOwnHead(PlayerSource source) {
+        Player sender = source.source();
+        return checkCooldown(sender).thenCompose(allowed -> {
+            if (allowed) {
+                return giveHeadInternal(sender, source.source());
+            }
+            return CompletableFuture.completedFuture(null);
+        });
+    }
 
     /**
      * Gives the player a head of the specified player
@@ -50,26 +64,37 @@ public class HeadCommand {
      */
     @Command("head|skull <player>")
     @CommandDescription("Gibt dir den Kopf eines Spielers")
-    @Permission("essentials.command.head")
+    @Permission("essentials.command.head.other")
     public CompletableFuture<Void> giveHead(PlayerSource source, OfflinePlayer player) {
         // check cooldown
         Player sender = source.source();
-        if (!source.source().hasPermission("essentials.cooldown.head.bypass")) {
-            return CompletableFuture.runAsync(() -> {
-                Duration duration = LAZY.INSTANCE.getCooldown(player.getUniqueId());
+        return checkCooldown(sender).thenCompose(allowed -> {
+            if (allowed) {
+                return giveHeadInternal(sender, player);
+            }
+            return CompletableFuture.completedFuture(null);
+        });
+    }
+
+    private CompletableFuture<Boolean> checkCooldown(Player sender) {
+        if (!sender.hasPermission("essentials.cooldown.head.bypass")) {
+            return CompletableFuture.supplyAsync(() -> {
+                Duration duration = LAZY.INSTANCE.getCooldown(sender.getUniqueId());
                 if (duration != null) {
                     sender.sendMessage(Text.deserialize("<prefix> <p>Du kannst in <duration> einen weiteren Kopf holen!",
                             Placeholder.component("duration", Text.durationComponent(duration))));
-                    return;
+                    return false;
                 }
-                int cooldownDays = PermissionsRange.min(source.source(), "essentials.cooldown.head");
+                int cooldownDays = PermissionsRange.min(sender, "essentials.cooldown.head");
                 if (cooldownDays == -1) {
-                    EssentialsPlugin.instance().getComponentLogger().warn("Player {}({}) has no cooldown permission configured and does not have a bypass", sender.getUniqueId(), sender.getName());
+                    EssentialsPlugin.instance().getComponentLogger().warn("Player {}({}) has no cooldown permission configured and does not have a bypass. Using 1 DAY", sender.getUniqueId(), sender.getName());
+                    cooldownDays = 1;
                 }
                 LAZY.INSTANCE.setCooldown(sender.getUniqueId(), Duration.ofDays(cooldownDays));
+                return true;
             }, EssentialsPlugin.getExecutorService());
         }
-        return giveHeadInternal(sender, player);
+        return CompletableFuture.completedFuture(true);
     }
 
     private CompletableFuture<Void> giveHeadInternal(Player sender, OfflinePlayer player) {
